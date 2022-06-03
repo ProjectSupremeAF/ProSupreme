e9ca48da8b4f7dde6643f76e6fbd18dec0f7ee95
namal <namalpathum28@gmail.com>
2022-06-03 17:12:51 +0530

    implement panel_member frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import Home from './components/Home';
+import UpdateItem from './components/UpdateItem';
+import Header from './components/Header';
+import AddItem from './components/AddItem';
+import DeleteItem from './components/DeleteItem';
+import GetOne from './components/GetOne';
 
-const App = () => {
-    return(
-        <div>
-            <h1>App</h1>
-            <h1>NAMAL PATHUM</h1>
-        </div>
-    );
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+   function App() {
+     return (
+       <Router>
+       <div className="App">
+      
+      <h1> Presentation Evaluation </h1>
+
+      
+
+      <Header />
+      
+     <Route path="/add" exact component={AddItem} />
+     <Route path="/" exact component={Home} /> 
+     <Route path="/update/:id" exact component={UpdateItem} />
+     <Route path="/delete/:id" exact component={DeleteItem} /> 
+     <Route path="/one/:id" exact component={GetOne} />
+
+    </div>
+    </Router>
+  );
 }
 
 export default App;
